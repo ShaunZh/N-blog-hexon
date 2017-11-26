a6bdd0d0e7cce199026b83bb5931b89ecd6bf3c4
zhangjie <zhangjie@11wlw.com>
2017-11-26 22:43:03 +0800

    添加登出功能

--- routes/signout.js
+++ routes/signout.js
@@ -5,7 +5,10 @@ const checkLogin = require('../middlewares/check').checkLogin
 
 // GET /signout 登出
 router.get('/', checkLogin, function (req, res, next) {
-  res.send('登出')
+  req.session.user = null // 登出就是讲session.user字段设置为null
+  req.flash('success', '登出成功')
+  // 登出成功后跳转到主页
+  res.redirect('/posts')
 })
 
 module.exports = router
