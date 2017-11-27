3b408f458db0a064a6c84ebfb4fba5a790256645
ShaunZh <308826842@qq.com>
2017-11-28 00:12:32 +0800

    添加发表文章页面 修改上传的用户名

--- routes/posts.js
+++ routes/posts.js
@@ -3,6 +3,8 @@ const router = express.Router()
 
 const checkLogin = require('../middlewares/check').checkLogin
 
+const PostModel = require('../models/posts')
+
 // GET /posts 所有用户或特定用户的文章页
 // eg: GET /posts?author=xxx
 router.get('/', function (req, res, next) {
@@ -10,17 +12,49 @@ router.get('/', function (req, res, next) {
 })
 
 // POST /posts/create 发表一篇文章
-router.post('/create', checkLogin, function (req, res, next) {
-  res.send('发表文章')
+router.post('/', checkLogin, function (req, res, next) {
+  const author = req.session.user._id
+  const title = req.fields.title
+  const content = req.fields.content
+  // 校验参数
+  try {
+    if (!title.length) {
+      throw new Error('请填写标题')
+    }
+    if (!content.length) {
+      throw new Error('请填写内容')
+    }
+  } catch (e) { // 参数错误
+    req.flash('error', e.message)
+    return res.redirect('back')
+  }
+
+  let post = {
+    author: author,
+    title: title,
+    content: content,
+    pv: 0
+  }
+
+  PostModel.create(post)
+    .then(function (result) {
+      // 此 post 是插入 mongodb 后的值，包含 _id
+      post = result.ops[0]
+      req.flash('success', '发表成功')
+      // 发表成功后跳转到该文章页
+      res.redirect(`/post/${post._id}`)
+    })
+    .catch(next)
 })
 
 // GET /posts/create 发表文章页
 router.get('/create', checkLogin, function (req, res, next) {
-  res.send('文章详情页')
+  res.render('create')
 })
 
 // GET /posts/:postId 单独一篇文章页
 router.get('/:postId', function (req, res, next) {
+  console.log('/:postId')
   res.send('文章详情页')
 })
 
